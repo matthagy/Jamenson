d65f3300642dd846ab5bff824849c9a2f8791548
Matt Hagy <hagy@gatech.edu>
2011-09-09 16:45:53 -0400

    Properly set stacklevel in deprecation warning

--- runtime/tmpio.py
+++ runtime/tmpio.py
@@ -8,7 +8,7 @@ from hlab.tempfile import temp_file_proxy as hlab_temp_file_proxy
 
 def temp_file_proxy(*args, **kwds):
     warnings.warn("temp_file_proxy is deprecated; moved to hlab.tempfile",
-                  DeprecationWarning)
+                  DeprecationWarning, stacklevel=2)
     return hlab_temp_file_proxy(*args, **kwds)
 
 
